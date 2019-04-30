a04f7445bd2779228046b0bbdb810d21939f8701
Remy Nicol <rnicol@live.fr>
2019-04-30 11:43:38 +0200

    linux: thread list

--- src/icebox/samples/linux/main.cpp
+++ src/icebox/samples/linux/main.cpp
@@ -6,6 +6,41 @@
 
 #include <icebox/linux/map.hpp>
 
+void display_thread(const core::Core& core, const thread_t& thread)
+{
+    const auto thread_id = core.os->thread_id({}, thread);
+
+    LOG(INFO, "thread: {:#x} id:{}",
+        thread.id,
+        (thread_id <= 4194304) ? std::to_string(thread_id) : "no");
+}
+
+void display_proc(const core::Core& core, const proc_t& proc)
+{
+    const auto proc_pid  = core.os->proc_id(proc);
+    const auto proc_name = core.os->proc_name(proc);
+
+    std::string threads;
+    int threads_count = -1;
+    core.os->thread_list(proc, [&](thread_t thread)
+    {
+        if(threads_count++ < 0)
+            return WALK_NEXT;
+
+        if(threads_count > 1)
+            threads.append(", ");
+
+        threads.append(std::to_string(core.os->thread_id({}, thread)));
+        return WALK_NEXT;
+    });
+
+    LOG(INFO, "process: {:#x} pid:{} '{}' {}",
+        proc.id,
+        (proc_pid <= 4194304) ? std::to_string(proc_pid) : "no",
+        (proc_name) ? *proc_name : "<noname>",
+        (threads_count > 0) ? "+" + std::to_string(threads_count) + " threads (" + threads + ")" : "");
+}
+
 int main(int argc, char** argv)
 {
     logg::init(argc, argv);
@@ -26,9 +61,7 @@ int main(int argc, char** argv)
     core.state.pause();
     core.os->proc_list([&](proc_t proc)
     {
-        const auto proc_pid  = core.os->proc_id(proc);
-        const auto proc_name = core.os->proc_name(proc);
-        LOG(INFO, "proc: {:#x} pid:{} '{}'", proc.id, proc_pid ? proc_pid : 0, proc_name ? proc_name->data() : "<noname>");
+        display_proc(core, proc);
         return WALK_NEXT;
     });
     core.state.resume();
@@ -40,27 +73,13 @@ int main(int argc, char** argv)
         core.state.pause();
         const auto thread = core.os->thread_current();
         if(thread)
-        {
-            const auto thread_id = core.os->thread_id({}, *thread);
-
-            LOG(INFO, "current thread: {:#x} id:{}",
-                (*thread).id,
-                (thread_id <= 4194304) ? std::to_string(thread_id) : "no");
-        }
+            display_thread(core, *thread);
         else
             LOG(ERROR, "no current thread");
 
         const auto proc = core.os->proc_current();
         if(proc)
-        {
-            const auto proc_pid  = core.os->proc_id(*proc);
-            const auto proc_name = core.os->proc_name(*proc);
-
-            LOG(INFO, "current proc: {:#x} pid:{} '{}'",
-                (*proc).id,
-                (proc_pid <= 4194304) ? std::to_string(proc_pid) : "no",
-                (proc_name) ? *proc_name : "<noname>");
-        }
+            display_proc(core, *proc);
         else
             LOG(ERROR, "no current proc");
         core.state.resume();
