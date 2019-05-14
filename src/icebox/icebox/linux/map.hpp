261f23003d536303a80264b8d2a7c9e11786824f
Remy Nicol <rnicol@live.fr>
2019-05-14 15:32:06 +0200

    linux: move kaslr management in System.map reader

--- src/icebox/icebox/linux/map.hpp
+++ src/icebox/icebox/linux/map.hpp
@@ -4,16 +4,16 @@
 
 namespace sym
 {
-    std::unique_ptr<IMod> make_map(span_t span, const std::string& module, const std::string& guid);
-
     struct Map
         : public sym::IMod
     {
-         Map(fs::path filename);
+         Map(fs::path path);
         ~Map();
 
         // methods
-        bool setup();
+        bool            setup   ();
+        bool            set_aslr(const std::string& symbol, const uint64_t addr);
+        opt<uint64_t>   get_aslr();
 
         // IModule methods
         span_t              span        () override;
@@ -22,8 +22,8 @@ namespace sym
         opt<size_t>         struc_size  (const std::string& struc) override;
         opt<sym::ModCursor> symbol      (uint64_t addr) override;
         bool                sym_list    (sym::on_sym_fn on_sym) override;
-
-        struct Data;
-        std::unique_ptr<Data> d_;
     };
+
+    std::unique_ptr<sym::Map> make_map(span_t span, const std::string& module, const std::string& guid);
+
 } // namespace sym
