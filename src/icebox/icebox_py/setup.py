c4049b6824f4eef2578f3cedeff0a7d1515ec38e
Benoît Amiaux <benoit.amiaux@gmail.com>
2019-12-16 14:13:54 +0100

    pybox: add setup.py

---
+++ src/icebox/icebox_py/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='icebox',
+      version='1.0',
+      description='Icebox VMI bindings',
+      author='Benoît Amiaux',
+      author_email='benoit.amiaux@gmail.com',
+      url='https://github.com/thalium/icebox',
+      packages=['icebox'],
+      package_data={
+          'icebox': ['icebox/_icebox.pyd'],
+      }
+      )
