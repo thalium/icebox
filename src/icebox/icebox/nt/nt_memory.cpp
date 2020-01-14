389cfd31cd412bc6f48a7658fac40ffb8376f02e
Jean-Marie Borello <borello.jeanmarie@gmail.com>
2020-01-14 16:36:02 +0100

    nt: add pte introspection

--- src/icebox/icebox/nt/nt_memory.cpp
+++ src/icebox/icebox/nt/nt_memory.cpp
@@ -24,13 +24,113 @@ namespace
         return ~(~uint64_t(0) << bits);
     }
 
+    // we will need to call this function recursively...
+    opt<ntphy_t> virtual_to_physical(nt::Os& os, uint64_t ptr, proc_t* proc, dtb_t dtb);
+
+    void unswizzle_pte(nt::Os& os, MMPTE& pte)
+    {
+        // The algorithm seems a bit strange but the following pattern is mostly used when a page fault occurs...
+        //
+        // if(PhysicalMemoryLimitMask && !(PteValue & 0x10))
+        // 	  PteValue = PteValue & ~PhysicalMemoryLimitMask;
+        if(os.PhysicalMemoryLimitMask_ && !pte.u.soft.SwizzleBit)
+            pte.u.value &= ~os.PhysicalMemoryLimitMask_;
+    }
+
     ntphy_t physical_page(uint64_t pfn, uint64_t offset)
     {
         return ntphy_t{pfn + offset, true, false};
     }
 
     constexpr auto page_fault_required = ntphy_t{0, false, false};
-    // constexpr auto zero_page        = ntphy_t{0, false, true};
+    constexpr auto zero_page           = ntphy_t{0, false, true};
+
+    ntphy_t prototype_pte_to_physical(nt::Os& os, uint64_t ptr, MMPTE pte)
+    {
+        const auto virt = virt_t{read_le64(&ptr)};
+        if(pte.u.hard.Valid)
+            return physical_page(pte.u.hard.PageFrameNumber * PAGE_SIZE, virt.u.f.offset);
+
+        unswizzle_pte(os, pte);
+        if(pte.u.soft.Prototype)
+            return page_fault_required; // FileMapping (SUBSECTION)
+
+        if(pte.u.soft.Transition)
+            return physical_page(pte.u.trans.PageFrameNumber * PAGE_SIZE, virt.u.f.offset);
+
+        if(!pte.u.soft.PageFileHigh)
+            return zero_page;
+
+        return page_fault_required; // pagefile
+    }
+
+    opt<ntphy_t> vad_pte_to_physical(nt::Os& os, uint64_t ptr, proc_t* proc)
+    {
+        if(!proc)
+        {
+            // invalid access for a kernel address
+            if(os.is_kernel_address(ptr))
+                return {};
+
+            // cannot access VAD without a proc
+            return page_fault_required;
+        }
+
+        const auto area = vm_area::find(os.core_, *proc, ptr);
+        if(!area)
+            return {};
+
+        const auto area_span = vm_area::span(os.core_, *proc, *area);
+        if(!area_span)
+            return {};
+
+        auto first_proto_pte = uint64_t{};
+        auto ok              = memory::read_virtual_with_dtb(os.core_, proc->kdtb, &first_proto_pte, area->id + os.offsets_[MMVAD_FirstPrototypePte], sizeof first_proto_pte);
+        if(!ok)
+            return {};
+
+        auto pte           = MMPTE{};
+        const auto pte_ptr = first_proto_pte + ((ptr - area_span->addr) / PAGE_SIZE) * sizeof pte;
+        ok                 = memory::read_virtual_with_dtb(os.core_, proc->kdtb, &pte.u.value, pte_ptr, sizeof pte.u.value);
+        if(!ok)
+            return {};
+
+        return prototype_pte_to_physical(os, ptr, pte);
+    }
+
+    opt<ntphy_t> pte_to_physical(nt::Os& os, uint64_t ptr, proc_t* proc, MMPTE pte)
+    {
+        const auto virt = virt_t{read_le64(&ptr)};
+        if(pte.u.hard.Valid)
+            return physical_page(pte.u.hard.PageFrameNumber * PAGE_SIZE, virt.u.f.offset);
+
+        unswizzle_pte(os, pte);
+        if(pte.u.soft.Prototype)
+        {
+            constexpr auto vad_mask = 0xFFFFFFFF00000000ULL;
+            if((pte.u.proto.ProtoAddress & vad_mask) == vad_mask)
+                return vad_pte_to_physical(os, ptr, proc);
+
+            // resolve prototype PTE from PagedPool so always use the kernel dtb
+            const auto addr = static_cast<uint64_t>(pte.u.proto.ProtoAddress);
+            const auto ok   = memory::read_virtual_with_dtb(os.core_, os.kernel_dtb(), &pte, addr, sizeof pte);
+            if(!ok)
+                return page_fault_required; // unable to read the prototype PTE in PagedPool
+
+            return prototype_pte_to_physical(os, ptr, pte);
+        }
+
+        if(pte.u.soft.Transition)
+            return physical_page(pte.u.trans.PageFrameNumber * PAGE_SIZE, virt.u.f.offset);
+
+        if(!pte.u.value)
+            return vad_pte_to_physical(os, ptr, proc);
+
+        if(!pte.u.soft.PageFileHigh)
+            return zero_page;
+
+        return page_fault_required; // pagefile
+    }
 
     opt<MMPTE> read_pml4e(nt::Os& os, const virt_t& virt, dtb_t dtb)
     {
@@ -39,6 +139,9 @@ namespace
         auto pml4e            = MMPTE{};
         const auto ok         = memory::read_physical(os.core_, &pml4e, pml4e_ptr, sizeof pml4e);
         if(!ok)
+            return {};
+
+        if(!pml4e.u.hard.Valid)
             return {};
 
         return pml4e;
@@ -50,6 +153,9 @@ namespace
         const auto pdpe_ptr = pml4e.u.hard.PageFrameNumber * PAGE_SIZE + virt.u.f.pdp * sizeof pdpe;
         auto ok             = memory::read_physical(os.core_, &pdpe, pdpe_ptr, sizeof pdpe);
         if(!ok)
+            return {};
+
+        if(!pdpe.u.hard.Valid)
             return {};
 
         return pdpe;
@@ -77,22 +183,16 @@ namespace
         return pde;
     }
 
-    opt<ntphy_t> virtual_to_physical(nt::Os& os, uint64_t ptr, proc_t* /*proc*/, dtb_t dtb)
+    opt<ntphy_t> virtual_to_physical(nt::Os& os, uint64_t ptr, proc_t* proc, dtb_t dtb)
     {
         const auto virt  = virt_t{read_le64(&ptr)};
         const auto pml4e = read_pml4e(os, virt, dtb);
         if(!pml4e)
             return {};
 
-        if(!pml4e->u.hard.Valid)
-            return page_fault_required;
-
         const auto pdpe = read_pdpe(os, virt, *pml4e);
         if(!pdpe)
             return {};
-
-        if(!pdpe->u.hard.Valid)
-            return page_fault_required;
 
         // 1g page
         if(pdpe->u.hard.LargePage)
@@ -107,7 +207,7 @@ namespace
             return {};
 
         if(!pde->u.hard.Valid)
-            return page_fault_required;
+            return pte_to_physical(os, ptr, proc, *pde);
 
         // 2mb page
         if(pde->u.hard.LargePage)
@@ -121,10 +221,7 @@ namespace
         if(!pte)
             return {};
 
-        if(!pte->u.hard.Valid)
-            return page_fault_required;
-
-        return physical_page(pte->u.hard.PageFrameNumber * PAGE_SIZE, virt.u.f.offset);
+        return pte_to_physical(os, ptr, proc, *pte);
     }
 
     enum class irql_e
