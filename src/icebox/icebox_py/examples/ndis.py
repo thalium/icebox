a2be2e4f67770bcf028191a23968be39f0701214
Benoît Amiaux <benoit.amiaux@gmail.com>
2020-01-22 17:07:50 +0100

    pybox: add ndis example

---
+++ src/icebox/icebox_py/examples/ndis.py
@@ -0,0 +1,17 @@
+import icebox
+
+vm = icebox.attach("win10")
+vm.symbols.load_drivers()
+proc = vm.processes.current()
+addr = proc.symbols.address("ndis!NdisSendNetBufferLists")
+phy = proc.memory.physical_address(addr)
+
+def on_break():
+    p = vm.processes.current()
+    p.symbols.load_modules()
+    p.symbols.dump_type("ndis!_NET_BUFFER_LIST", vm.registers.rdx)
+    for addr in p.callstacks():
+        print(p.symbols.string(addr))
+
+with vm.break_on_physical("", phy, on_break):
+    vm.exec()
