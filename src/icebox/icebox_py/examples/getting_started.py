6a19f35378e9b4166098393720ca5899c08b1db4
Benoît Amiaux <benoit.amiaux@gmail.com>
2020-01-28 17:35:52 +0100

    pybox: improve getting_started example

--- src/icebox/icebox_py/examples/getting_started.py
+++ src/icebox/icebox_py/examples/getting_started.py
@@ -1,22 +1,22 @@
 import icebox
 
 vm = icebox.attach("win10")  # attach to vm named "win10"
-proc = vm.processes.find_name("Taskmgr.exe")  # find process named 'Taskmgr'
+proc = vm.processes.find_name("dwm.exe")  # find process named 'dwm'
 print("%s pid:%d" % (proc.name(), proc.pid()))
-proc.symbols.load_modules()  # load all Taskmgr module symbols
+for mod in ["kernel32", "kernelbase"]:
+    proc.symbols.load_module(mod)  # load some symbols
 
-counter = icebox.counter()  # create a shared counter
+counter = icebox.counter()  # run the vm until we've updated this counter twice
 
 
-def print_taskmgr_callstack():  # callback called on every breakpoint
+def dump_callstack():  # dump the current proc callstack
     print()  # skip a line
-    for addr in proc.callstack():  # read current Taskmgr callstack
-        print(proc.symbols.string(addr))  # convert & print address
-    counter.add()
+    for addr in proc.callstack():  # read current dwm.exe callstack
+        print(proc.symbols.string(addr))  # convert & print callstack address
+    counter.add()  # update counter
 
 
-# set a breakpoint on ntdll!NtQuerySystemInformation
-addr = "ntdll!NtQuerySystemInformation"
-with vm.break_on_process(proc, addr, print_taskmgr_callstack):
-    while counter.read() < 2:  # run until callback is called twice
+# set a breakpoint on ntdll!NtWaitForSingleObject
+with vm.break_on_process(proc, "ntdll!NtWaitForSingleObject", dump_callstack):
+    while counter.read() < 2:  # run until dump_callstack is called twice
         vm.exec()
